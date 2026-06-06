ff61ae689325c34191df4d149c2915910aad4a52
sungjaepang <preadbear@gmail.com>
2026-06-06 20:46:26 +0900

    fix: use hash router for github pages

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,14 @@
 /* 11단계 TanStack Query 적용 (API 로직 실무형) */
 /* 12단계 Toast 알림 - 2. Toaster 추가 */
 /*  */
+/* + BrowserRouter를 HashRouter로 변경 */
 /*  */
 
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom"; /* 변경 전 */
+import { HashRouter } from "react-router-dom"; /* 변경 후 */
 // 11단계
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // 12단계
@@ -28,12 +30,12 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* 11단계 */}
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      {/* <BrowserRouter> */}
+      <HashRouter>
         <App />
         {/* 12단계 */}
         <Toaster position="top-center" />
-      </BrowserRouter>
-
+      </HashRouter>
     </QueryClientProvider>
   </StrictMode>,
 );
